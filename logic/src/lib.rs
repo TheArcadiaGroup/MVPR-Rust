b700db311d959a445e3cf4a8acd547def453d732
YessineAmor <yassine.amor@esprit.tn>
2021-03-29 08:18:57 +0100

    update error enum

--- logic/src/lib.rs
+++ logic/src/lib.rs
@@ -6,7 +6,7 @@ mod error;
 mod proposal;
 pub mod voting;
 pub mod custom_types;
-pub use error::{ProposalError, VotingError};
+pub use error::{ProposalError, VotingEngineError};
 
 pub use proposal::Proposal;
 // pub use voting::Voting;
