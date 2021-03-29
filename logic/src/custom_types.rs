fdda5267083e25b21f53c618654305ce688cd503
YessineAmor <yassine.amor@esprit.tn>
2021-03-29 08:16:25 +0100

    update custom types

--- logic/src/custom_types.rs
+++ logic/src/custom_types.rs
@@ -1,24 +1,17 @@
 #![no_std]
 extern crate alloc;
-use crate::error::*;
-use alloc::collections::BTreeMap;
-use alloc::string::String;
-use alloc::vec::Vec;
-use core::{
-    cmp::{Eq, Ord, PartialEq, PartialOrd},
-    ops::Add,
-};
-use types::{account::AccountHash, bytesrepr::FromBytes, PublicKey, U256};
+
 pub mod custom_types {
+    use alloc::collections::BTreeMap;
+    use alloc::string::String;
+    use alloc::vec::Vec;
+    use types::U256;
+
     pub type ProposalSerialized = (
         (
             // 0
             // name, storage_pointer, storage_fingerprint
-            (
-                alloc::string::String,
-                alloc::string::String,
-                alloc::string::String,
-            ), // 0.0
+            (String, String, String), // 0.0
             (
                 // 0.1
                 // proposal type
@@ -26,7 +19,7 @@ pub mod custom_types {
                 // proposer
                 [u8; 32],
                 // citations
-                alloc::vec::Vec<u64>,
+                Vec<u64>,
             ),
             (
                 // 0.2
@@ -45,23 +38,17 @@ pub mod custom_types {
             // Sponsors
             SponsorsSerialized, // 0.1
             // cost
-            types::U256, //0.2
+            U256, //0.2
         ),
     );
 
-    type MilestoneSerialized = alloc::collections::BTreeMap<
-        u64,
-        (
-            (u8, u8, u8),
-            alloc::collections::BTreeMap<u64, FundingTrancheSerialized>,
-            u64,
-        ),
-    >;
+    type MilestoneSerialized =
+        BTreeMap<u64, ((u8, u8, u8), BTreeMap<u64, FundingTrancheSerialized>, u64)>;
 
-    type SponsorsSerialized = alloc::collections::BTreeMap<[u8; 32], types::U256>;
+    type SponsorsSerialized = BTreeMap<[u8; 32], U256>;
 
-    type FundingTrancheSerialized = (u8, types::U256, types::U256);
+    type FundingTrancheSerialized = (u8, U256, U256);
 
     type RatiosSerialized = ([u8; 3]);
-    type VoteConfigurationSerialized = ((u64, u64), (u8, u8, u8));
+    type VoteConfigurationSerialized = ((u64, U256), (u8, u64, U256));
 }
